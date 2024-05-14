8fcd96e6c6e36bdef460712e997d45d7dd05e524
Guilherme <guilherme.benedito@fatec.sp.gov.br>
2024-05-14 19:03:16 -0300

    Criação da página com tabela de clientes cadastrados

--- app.py
+++ app.py
@@ -60,3 +60,30 @@ def addCliente():
         conn.close()
 
     return redirect('/cadastro')
+
+@app.route("/clientes")
+def mostrar_cliente():
+    conn = create_connection()
+    try:
+        cursor = conn.cursor() 
+        cursor.execute("SELECT * FROM cliente")
+        clientes = cursor.fetchall()
+        conn.commit()
+    except pymysql.MySQLError as e:
+        print(f"Erro ao executar a query: {e}")
+    finally:
+        conn.close()
+    return render_template("clientes.html", clientes=clientes)
+
+@app.route("/delete_cliente/<int:id>", methods=["POST"])
+def delete_cliente(id):
+    conn = create_connection()
+    try:
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM cliente WHERE id = %s", (id,))
+        conn.commit()
+    except pymysql.MySQLError as e:
+        print(f"Erro ao executar a query: {e}")
+    finally:
+        conn.close()
+    return redirect('/clientes')
